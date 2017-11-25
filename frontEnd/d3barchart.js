cae0743aff8468a53d889ed68795a78c396b2afd
Kripanshu Bhargava <kripanshubhargava@gmail.com>
2017-11-25 16:51:16 -0600

    barchart2

--- frontEnd/d3barchart.js
+++ frontEnd/d3barchart.js
@@ -486,6 +486,160 @@ var my_json=
   }
 }
 
+var my_json2={
+  "head": {
+    "vars": [ "g" , "c" , "average" ]
+  } ,
+  "results": {
+    "bindings": [
+      {
+        "g": { "type": "uri" , "value": "http://localhost:3030/payrolls/data/lacity" } ,
+        "c": { "type": "literal" , "value": "Office of Finance" } ,
+        "average": { "type": "literal" , "datatype": "http://www.w3.org/2001/XMLSchema#decimal" , "value": "33122.245" }
+      } ,
+      {
+        "g": { "type": "uri" , "value": "http://localhost:3030/payrolls/data/lacity" } ,
+        "c": { "type": "literal" , "value": "Economic And Workforce Development Department (EWDD)" } ,
+        "average": { "type": "literal" , "datatype": "http://www.w3.org/2001/XMLSchema#decimal" , "value": "67652.137142857142857142857142" }
+      } ,
+      {
+        "g": { "type": "uri" , "value": "http://localhost:3030/payrolls/data/lacity" } ,
+        "c": { "type": "literal" , "value": "Recreation And Parks" } ,
+        "average": { "type": "literal" , "datatype": "http://www.w3.org/2001/XMLSchema#decimal" , "value": "63809.98909090909090909090909" }
+      } ,
+      {
+        "g": { "type": "uri" , "value": "http://localhost:3030/payrolls/data/lacity" } ,
+        "c": { "type": "literal" , "value": "Animal Services" } ,
+        "average": { "type": "literal" , "datatype": "http://www.w3.org/2001/XMLSchema#decimal" , "value": "66690.58" }
+      } ,
+      {
+        "g": { "type": "uri" , "value": "http://localhost:3030/payrolls/data/lacity" } ,
+        "c": { "type": "literal" , "value": "Pension (LAFPP)" } ,
+        "average": { "type": "literal" , "datatype": "http://www.w3.org/2001/XMLSchema#decimal" , "value": "53259.104444444444444444444444" }
+      } ,
+      {
+        "g": { "type": "uri" , "value": "http://localhost:3030/payrolls/data/lacity" } ,
+        "c": { "type": "literal" , "value": "Fire (LAFD)" } ,
+        "average": { "type": "literal" , "datatype": "http://www.w3.org/2001/XMLSchema#decimal" , "value": "67281.06" }
+      } ,
+      {
+        "g": { "type": "uri" , "value": "http://localhost:3030/payrolls/data/lacity" } ,
+        "c": { "type": "literal" , "value": "Information Technology Agency" } ,
+        "average": { "type": "literal" , "datatype": "http://www.w3.org/2001/XMLSchema#decimal" , "value": "77555.32" }
+      } ,
+      {
+        "g": { "type": "uri" , "value": "http://localhost:3030/payrolls/data/lacity" } ,
+        "c": { "type": "literal" , "value": "General Services" } ,
+        "average": { "type": "literal" , "datatype": "http://www.w3.org/2001/XMLSchema#decimal" , "value": "65669.249130434782608695652173" }
+      } ,
+      {
+        "g": { "type": "uri" , "value": "http://localhost:3030/payrolls/data/nyc" } ,
+        "c": { "type": "literal" , "value": "POLICE DEPARTMENT" } ,
+        "average": { "type": "literal" , "datatype": "http://www.w3.org/2001/XMLSchema#decimal" , "value": "43165.9575" }
+      } ,
+      {
+        "g": { "type": "uri" , "value": "http://localhost:3030/payrolls/data/lacity" } ,
+        "c": { "type": "literal" , "value": "Los Angeles Department of Convention and Tourism Development" } ,
+        "average": { "type": "literal" , "datatype": "http://www.w3.org/2001/XMLSchema#decimal" , "value": "14360.675" }
+      } ,
+      {
+        "g": { "type": "uri" , "value": "http://localhost:3030/payrolls/data/lacity" } ,
+        "c": { "type": "literal" , "value": "Harbor (Port of LA)" } ,
+        "average": { "type": "literal" , "datatype": "http://www.w3.org/2001/XMLSchema#decimal" , "value": "67766.695" }
+      } ,
+      {
+        "g": { "type": "uri" , "value": "http://localhost:3030/payrolls/data/lacity" } ,
+        "c": { "type": "literal" , "value": "City Planning And Development (DCP)" } ,
+        "average": { "type": "literal" , "datatype": "http://www.w3.org/2001/XMLSchema#decimal" , "value": "66787.36" }
+      } ,
+      {
+        "g": { "type": "uri" , "value": "http://localhost:3030/payrolls/data/lacity" } ,
+        "c": { "type": "literal" , "value": "Public Works - Sanitation" } ,
+        "average": { "type": "literal" , "datatype": "http://www.w3.org/2001/XMLSchema#decimal" , "value": "66851.88" }
+      } ,
+      {
+        "g": { "type": "uri" , "value": "http://localhost:3030/payrolls/data/lacity" } ,
+        "c": { "type": "literal" , "value": "City Employees Retirement System (LACERS)" } ,
+        "average": { "type": "literal" , "datatype": "http://www.w3.org/2001/XMLSchema#decimal" , "value": "53349.82" }
+      } ,
+      {
+        "g": { "type": "uri" , "value": "http://localhost:3030/payrolls/data/lacity" } ,
+        "c": { "type": "literal" , "value": "Neighborhood Empowerment (DONE)" } ,
+        "average": { "type": "literal" , "datatype": "http://www.w3.org/2001/XMLSchema#decimal" , "value": "66335.72" }
+      } ,
+      {
+        "g": { "type": "uri" , "value": "http://localhost:3030/payrolls/data/lacity" } ,
+        "c": { "type": "literal" , "value": "El Pueblo De Los Angeles Historical Monument Authority" } ,
+        "average": { "type": "literal" , "datatype": "http://www.w3.org/2001/XMLSchema#decimal" , "value": "67903.8" }
+      } ,
+      {
+        "g": { "type": "uri" , "value": "http://localhost:3030/payrolls/data/lacity" } ,
+        "c": { "type": "literal" , "value": "Housing And Community Investment Department" } ,
+        "average": { "type": "literal" , "datatype": "http://www.w3.org/2001/XMLSchema#decimal" , "value": "51248.174166666666666666666666" }
+      } ,
+      {
+        "g": { "type": "uri" , "value": "http://localhost:3030/payrolls/data/lacity" } ,
+        "c": { "type": "literal" , "value": "Public Works - Board of Public Works" } ,
+        "average": { "type": "literal" , "datatype": "http://www.w3.org/2001/XMLSchema#decimal" , "value": "62613.699629629629629629629629" }
+      } ,
+      {
+        "g": { "type": "uri" , "value": "http://localhost:3030/payrolls/data/lacity" } ,
+        "c": { "type": "literal" , "value": "Controller" } ,
+        "average": { "type": "literal" , "datatype": "http://www.w3.org/2001/XMLSchema#decimal" , "value": "60338.2592" }
+      } ,
+      {
+        "g": { "type": "uri" , "value": "http://localhost:3030/payrolls/data/lacity" } ,
+        "c": { "type": "literal" , "value": "Library" } ,
+        "average": { "type": "literal" , "datatype": "http://www.w3.org/2001/XMLSchema#decimal" , "value": "41567.6" }
+      } ,
+      {
+        "g": { "type": "uri" , "value": "http://localhost:3030/payrolls/data/lacity" } ,
+        "c": { "type": "literal" , "value": "Zoo" } ,
+        "average": { "type": "literal" , "datatype": "http://www.w3.org/2001/XMLSchema#decimal" , "value": "33812.24" }
+      } ,
+      {
+        "g": { "type": "uri" , "value": "http://localhost:3030/payrolls/data/lacity" } ,
+        "c": { "type": "literal" , "value": "Transportation (LADOT)" } ,
+        "average": { "type": "literal" , "datatype": "http://www.w3.org/2001/XMLSchema#decimal" , "value": "65546.025" }
+      } ,
+      {
+        "g": { "type": "uri" , "value": "http://localhost:3030/payrolls/data/lacity" } ,
+        "c": { "type": "literal" , "value": "City Planning" } ,
+        "average": { "type": "literal" , "datatype": "http://www.w3.org/2001/XMLSchema#decimal" , "value": "35417.6" }
+      } ,
+      {
+        "g": { "type": "uri" , "value": "http://localhost:3030/payrolls/data/lacity" } ,
+        "c": { "type": "literal" , "value": "Los Angeles Convention Center" } ,
+        "average": { "type": "literal" , "datatype": "http://www.w3.org/2001/XMLSchema#decimal" , "value": "21209.235" }
+      } ,
+      {
+        "g": { "type": "uri" , "value": "http://localhost:3030/payrolls/data/lacity" } ,
+        "c": { "type": "literal" , "value": "Police (LAPD)" } ,
+        "average": { "type": "literal" , "datatype": "http://www.w3.org/2001/XMLSchema#decimal" , "value": "64313.3" }
+      } ,
+      {
+        "g": { "type": "uri" , "value": "http://localhost:3030/payrolls/data/lacity" } ,
+        "c": { "type": "literal" , "value": "Airports (LAWA)" } ,
+        "average": { "type": "literal" , "datatype": "http://www.w3.org/2001/XMLSchema#decimal" , "value": "61992.318518518518518518518518" }
+      } ,
+      {
+        "g": { "type": "uri" , "value": "http://localhost:3030/payrolls/data/lacity" } ,
+        "c": { "type": "literal" , "value": "Building and Safety" } ,
+        "average": { "type": "literal" , "datatype": "http://www.w3.org/2001/XMLSchema#decimal" , "value": "60040.976666666666666666666666" }
+      } ,
+      {
+        "g": { "type": "uri" , "value": "http://localhost:3030/payrolls/data/nyc" } ,
+        "c": { "type": "literal" , "value": "DISTRICT ATTORNEY-MANHATTAN   " } ,
+        "average": { "type": "literal" , "datatype": "http://www.w3.org/2001/XMLSchema#decimal" , "value": "65840.969012096774193548387096" }
+      } ,
+      {
+        "g": { "type": "uri" , "value": "http://localhost:3030/payrolls/data/lacity" } ,
+        "c": { "type": "literal" , "value": "Aging" } ,
+        "average": { "type": "literal" , "datatype": "http://www.w3.org/2001/XMLSchema#decimal" , "value": "41003.6275" }
+      }
+    ]
+  }
+}
 
 ;
 $(document).ready(function(){
@@ -494,6 +648,13 @@ $(document).ready(function(){
 
 
         barChart(my_json);
+
+    });
+    $("#onClick2").click(function(){
+        console.log("oh yeah2");
+
+
+        barChart2(my_json2);
 
     });
 });
@@ -592,6 +753,157 @@ console.log(d.y);
         .attr("y", function(d) { return y(d.y); })
         .attr("width", x.bandwidth())
         .attr("height", function(d) { return height - y(d.y); })
+
+      svg.append("text")
+          .attr("text-anchor", "middle")  // this makes it easy to centre the text as the transform is applied to the anchor
+          .attr("transform", "translate(" + padding / 5 + "," + (height / 2) + ")rotate(-90)")  // text is drawn off the screen top left, move down and out and rotate
+          .text("AverageSalary")
+          .style("fill", "#424242")
+          .style("text-indent","20px")
+          .style("font-size","12px")
+          .style("font-weight","bold");
+
+      svg.append("text")
+          .attr("text-anchor", "middle")  // this makes it easy to centre the text as the transform is applied to the anchor
+          .attr("transform", "translate(" + (width / 2) + "," + (height + (padding + padding )) + ")")  // centre below axis
+          .text("Department")
+          .style("fill", "#424242")
+          .style("text-indent","20px")
+          .style("font-size","12px")
+          .style("font-weight","bold");
+
+
+
+
+
+}
+
+
+//Department wise base salary LA vs NYC
+function barChart2(my_json2){
+var mydata = [];
+var padding=100;
+for (var i=0; i<my_json2.results.bindings.length; i++){
+//console.log(my_json.results.bindings[i]);
+//  console.log(my_json.results.bindings[i].o.value);
+//  console.log(my_json.results.bindings[i].avgSal.value);
+
+
+  // x_bar.push(my_json.results.bindings[i].o.value);
+  // y_bar.push(my_json.results.bindings[i].avgSal.value);
+
+mydata.push({x:my_json2.results.bindings[i].c.value, y:my_json2.results.bindings[i].average.value, z:splitString(my_json2.results.bindings[i].g.value)});
+
+}
+function splitString(s){
+  var result;
+  result = s.split("/");
+return result[result.length-1];
+};
+
+for (var i=0; i<mydata.length; i++){
+ console.log(mydata[i].x);
+ console.log(mydata[i].y);
+ console.log(mydata[i].z);
+}
+
+       // mydata.forEach(function (d) {
+       //
+       //   //console.log(d.x);
+       //   //console.log(d.y);
+       //     d.x = +d.x;
+       //     d.y = +d.y;
+       // });
+
+
+  var svg = d3.select("svg"),
+      margin = {top: 10, right: 20, bottom: 220, left: 90},
+      width = +svg.attr("width") - margin.left - margin.right,
+      height = +svg.attr("height") - margin.top - margin.bottom;
+
+  var x = d3.scaleBand().rangeRound([0, width]).padding(0.1),
+      y = d3.scaleLinear().rangeRound([height, 0]);
+
+  var g = svg.append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+
+    x.domain(mydata.map(function(d,i) {
+console.log(d.x);
+      return d.x;
+
+     }));
+    y.domain([0, d3.max(mydata, function(d) {
+console.log(d.y);
+      return d.y;
+
+    })]);
+
+
+
+    // g.append("g")
+    //     .attr("class", "axis axis--x")
+    //     .attr("transform", "translate(0," + height + ")")
+    //     .call(d3.axisBottom(x));
+
+        g.append("g")
+			.classed("x axis", true)
+			.attr("transform", "translate(" + 0 + "," + height + ")")
+			.call(d3.axisBottom(x))
+				.selectAll("text")
+					.classed("x-axis-label", true)
+					.style("text-anchor", "end")
+          .attr("font-size","9px")
+					.attr("dx", -8)
+					.attr("dy", 8)
+					.attr("transform", "translate(0,0) rotate(-45)")
+          .append("text")
+          .text(function(d,i){
+            return " | "+mydata[i].z;
+          })
+          .style("text-anchor", "end")
+          .attr("font-size","9px")
+					.attr("dx", -8)
+					.attr("dy", 8)
+					.attr("transform", "translate(0,0) rotate(-45)")
+
+          ;
+
+    g.append("g")
+        .attr("class", "y axis")
+        .call(d3.axisLeft(y).ticks(5))
+      .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", "0.71em")
+        .attr("text-anchor", "end")
+        .text("Base Salary")
+      ;
+
+    g.selectAll(".bar")
+      .data(mydata)
+      .enter().append("rect")
+        .attr("class", "bar")
+        .attr("x", function(d) { return x(d.x); })
+        .attr("y", function(d) { return y(d.y); })
+        .attr("width", x.bandwidth())
+        .attr("height", function(d) { return height - y(d.y); })
+        .attr("fill",function(d,i){
+
+          console.log("what to do"+d.z);
+          if(d.z==="lacity")
+          {
+            console.log("red");
+            return "#E57373";
+          }
+          else{
+            console.log("blue");
+            return "steelblue";
+          }
+
+        })
+        ;
 
       svg.append("text")
           .attr("text-anchor", "middle")  // this makes it easy to centre the text as the transform is applied to the anchor
