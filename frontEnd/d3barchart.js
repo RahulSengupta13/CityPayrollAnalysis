59cf68a05577f3a5cbd0d00b5b8251ebdfa9143c
Kripanshu Bhargava <kripanshubhargava@gmail.com>
2017-11-25 14:10:26 -0600

    d3codepush

---
+++ frontEnd/d3barchart.js
@@ -0,0 +1,592 @@
+
+var my_json=
+{
+  "head": {
+    "vars": [
+      "o",
+      "avgSal"
+    ]
+  },
+  "results": {
+    "bindings": [
+      {
+        "o": {
+          "type": "literal",
+          "value": "Planning"
+        },
+        "avgSal": {
+          "type": "literal",
+          "datatype": "http://www.w3.org/2001/XMLSchema#decimal",
+          "value": "31.865"
+        }
+      },
+      {
+        "o": {
+          "type": "literal",
+          "value": "Housing And Community Investment Department"
+        },
+        "avgSal": {
+          "type": "literal",
+          "datatype": "http://www.w3.org/2001/XMLSchema#decimal",
+          "value": "31.0125"
+        }
+      },
+      {
+        "o": {
+          "type": "literal",
+          "value": "Controller"
+        },
+        "avgSal": {
+          "type": "literal",
+          "datatype": "http://www.w3.org/2001/XMLSchema#decimal",
+          "value": "31.814042553191489361702127"
+        }
+      },
+      {
+        "o": {
+          "type": "literal",
+          "value": "Library"
+        },
+        "avgSal": {
+          "type": "literal",
+          "datatype": "http://www.w3.org/2001/XMLSchema#decimal",
+          "value": "29.545"
+        }
+      },
+      {
+        "o": {
+          "type": "literal",
+          "value": "Public Works - Board of Public Works"
+        },
+        "avgSal": {
+          "type": "literal",
+          "datatype": "http://www.w3.org/2001/XMLSchema#decimal",
+          "value": "32.55037037037037037037037"
+        }
+      },
+      {
+        "o": {
+          "type": "literal",
+          "value": "City Employees Retirement System"
+        },
+        "avgSal": {
+          "type": "literal",
+          "datatype": "http://www.w3.org/2001/XMLSchema#decimal",
+          "value": "31.944"
+        }
+      },
+      {
+        "o": {
+          "type": "literal",
+          "value": "Community Development Department"
+        },
+        "avgSal": {
+          "type": "literal",
+          "datatype": "http://www.w3.org/2001/XMLSchema#decimal",
+          "value": "31.865"
+        }
+      },
+      {
+        "o": {
+          "type": "literal",
+          "value": "Water And Power"
+        },
+        "avgSal": {
+          "type": "literal",
+          "datatype": "http://www.w3.org/2001/XMLSchema#decimal",
+          "value": "24.306666666666666666666666"
+        }
+      },
+      {
+        "o": {
+          "type": "literal",
+          "value": "Neighborhood Empowerment (DONE)"
+        },
+        "avgSal": {
+          "type": "literal",
+          "datatype": "http://www.w3.org/2001/XMLSchema#decimal",
+          "value": "32.74"
+        }
+      },
+      {
+        "o": {
+          "type": "literal",
+          "value": "City Employees Retirement System (LACERS)"
+        },
+        "avgSal": {
+          "type": "literal",
+          "datatype": "http://www.w3.org/2001/XMLSchema#decimal",
+          "value": "29.718888888888888888888888"
+        }
+      },
+      {
+        "o": {
+          "type": "literal",
+          "value": "Los Angeles Housing"
+        },
+        "avgSal": {
+          "type": "literal",
+          "datatype": "http://www.w3.org/2001/XMLSchema#decimal",
+          "value": "31.559117647058823529411764"
+        }
+      },
+      {
+        "o": {
+          "type": "literal",
+          "value": "El Pueblo De Los Angeles Historical Monument Authority"
+        },
+        "avgSal": {
+          "type": "literal",
+          "datatype": "http://www.w3.org/2001/XMLSchema#decimal",
+          "value": "32.5275"
+        }
+      },
+      {
+        "o": {
+          "type": "literal",
+          "value": "Police"
+        },
+        "avgSal": {
+          "type": "literal",
+          "datatype": "http://www.w3.org/2001/XMLSchema#decimal",
+          "value": "30.28625"
+        }
+      },
+      {
+        "o": {
+          "type": "literal",
+          "value": "Public Works - Sanitation"
+        },
+        "avgSal": {
+          "type": "literal",
+          "datatype": "http://www.w3.org/2001/XMLSchema#decimal",
+          "value": "32.156666666666666666666666"
+        }
+      },
+      {
+        "o": {
+          "type": "literal",
+          "value": "Pension"
+        },
+        "avgSal": {
+          "type": "literal",
+          "datatype": "http://www.w3.org/2001/XMLSchema#decimal",
+          "value": "31.865"
+        }
+      },
+      {
+        "o": {
+          "type": "literal",
+          "value": "Fire"
+        },
+        "avgSal": {
+          "type": "literal",
+          "datatype": "http://www.w3.org/2001/XMLSchema#decimal",
+          "value": "31.052"
+        }
+      },
+      {
+        "o": {
+          "type": "literal",
+          "value": "Building and Safety"
+        },
+        "avgSal": {
+          "type": "literal",
+          "datatype": "http://www.w3.org/2001/XMLSchema#decimal",
+          "value": "33.04"
+        }
+      },
+      {
+        "o": {
+          "type": "literal",
+          "value": "Aging"
+        },
+        "avgSal": {
+          "type": "literal",
+          "datatype": "http://www.w3.org/2001/XMLSchema#decimal",
+          "value": "30.123846153846153846153846"
+        }
+      },
+      {
+        "o": {
+          "type": "literal",
+          "value": "Airports (LAWA)"
+        },
+        "avgSal": {
+          "type": "literal",
+          "datatype": "http://www.w3.org/2001/XMLSchema#decimal",
+          "value": "31.879259259259259259259259"
+        }
+      },
+      {
+        "o": {
+          "type": "literal",
+          "value": "Airports"
+        },
+        "avgSal": {
+          "type": "literal",
+          "datatype": "http://www.w3.org/2001/XMLSchema#decimal",
+          "value": "30.40125"
+        }
+      },
+      {
+        "o": {
+          "type": "literal",
+          "value": "Los Angeles Convention Center"
+        },
+        "avgSal": {
+          "type": "literal",
+          "datatype": "http://www.w3.org/2001/XMLSchema#decimal",
+          "value": "27.921"
+        }
+      },
+      {
+        "o": {
+          "type": "literal",
+          "value": "Police (LAPD)"
+        },
+        "avgSal": {
+          "type": "literal",
+          "datatype": "http://www.w3.org/2001/XMLSchema#decimal",
+          "value": "31.8975"
+        }
+      },
+      {
+        "o": {
+          "type": "literal",
+          "value": "Neighborhood Empowerment"
+        },
+        "avgSal": {
+          "type": "literal",
+          "datatype": "http://www.w3.org/2001/XMLSchema#decimal",
+          "value": "31.865"
+        }
+      },
+      {
+        "o": {
+          "type": "literal",
+          "value": "Zoo"
+        },
+        "avgSal": {
+          "type": "literal",
+          "datatype": "http://www.w3.org/2001/XMLSchema#decimal",
+          "value": "25.21"
+        }
+      },
+      {
+        "o": {
+          "type": "literal",
+          "value": "Harbor"
+        },
+        "avgSal": {
+          "type": "literal",
+          "datatype": "http://www.w3.org/2001/XMLSchema#decimal",
+          "value": "31.865"
+        }
+      },
+      {
+        "o": {
+          "type": "literal",
+          "value": "City Planning"
+        },
+        "avgSal": {
+          "type": "literal",
+          "datatype": "http://www.w3.org/2001/XMLSchema#decimal",
+          "value": "28.97"
+        }
+      },
+      {
+        "o": {
+          "type": "literal",
+          "value": "Public Works - Board Of Public Works"
+        },
+        "avgSal": {
+          "type": "literal",
+          "datatype": "http://www.w3.org/2001/XMLSchema#decimal",
+          "value": "31.8808"
+        }
+      },
+      {
+        "o": {
+          "type": "literal",
+          "value": "Transportation (LADOT)"
+        },
+        "avgSal": {
+          "type": "literal",
+          "datatype": "http://www.w3.org/2001/XMLSchema#decimal",
+          "value": "32.74"
+        }
+      },
+      {
+        "o": {
+          "type": "literal",
+          "value": "Fire (LAFD)"
+        },
+        "avgSal": {
+          "type": "literal",
+          "datatype": "http://www.w3.org/2001/XMLSchema#decimal",
+          "value": "32.965"
+        }
+      },
+      {
+        "o": {
+          "type": "literal",
+          "value": "Office Of Finance"
+        },
+        "avgSal": {
+          "type": "literal",
+          "datatype": "http://www.w3.org/2001/XMLSchema#decimal",
+          "value": "31.733333333333333333333333"
+        }
+      },
+      {
+        "o": {
+          "type": "literal",
+          "value": "Animal Services"
+        },
+        "avgSal": {
+          "type": "literal",
+          "datatype": "http://www.w3.org/2001/XMLSchema#decimal",
+          "value": "32.5"
+        }
+      },
+      {
+        "o": {
+          "type": "literal",
+          "value": "Pension (LAFPP)"
+        },
+        "avgSal": {
+          "type": "literal",
+          "datatype": "http://www.w3.org/2001/XMLSchema#decimal",
+          "value": "29.874444444444444444444444"
+        }
+      },
+      {
+        "o": {
+          "type": "literal",
+          "value": "Economic And Workforce Development Department (EWDD)"
+        },
+        "avgSal": {
+          "type": "literal",
+          "datatype": "http://www.w3.org/2001/XMLSchema#decimal",
+          "value": "33.125714285714285714285714"
+        }
+      },
+      {
+        "o": {
+          "type": "literal",
+          "value": "Building And Safety"
+        },
+        "avgSal": {
+          "type": "literal",
+          "datatype": "http://www.w3.org/2001/XMLSchema#decimal",
+          "value": "31.865"
+        }
+      },
+      {
+        "o": {
+          "type": "literal",
+          "value": "Recreation And Parks"
+        },
+        "avgSal": {
+          "type": "literal",
+          "datatype": "http://www.w3.org/2001/XMLSchema#decimal",
+          "value": "31.8996875"
+        }
+      },
+      {
+        "o": {
+          "type": "literal",
+          "value": "Office of Finance"
+        },
+        "avgSal": {
+          "type": "literal",
+          "datatype": "http://www.w3.org/2001/XMLSchema#decimal",
+          "value": "24.18"
+        }
+      },
+      {
+        "o": {
+          "type": "literal",
+          "value": "Los Angeles Department of Convention and Tourism Development"
+        },
+        "avgSal": {
+          "type": "literal",
+          "datatype": "http://www.w3.org/2001/XMLSchema#decimal",
+          "value": "23.88"
+        }
+      },
+      {
+        "o": {
+          "type": "literal",
+          "value": "Harbor (Port of LA)"
+        },
+        "avgSal": {
+          "type": "literal",
+          "datatype": "http://www.w3.org/2001/XMLSchema#decimal",
+          "value": "33.19"
+        }
+      },
+      {
+        "o": {
+          "type": "literal",
+          "value": "City Planning And Development (DCP)"
+        },
+        "avgSal": {
+          "type": "literal",
+          "datatype": "http://www.w3.org/2001/XMLSchema#decimal",
+          "value": "32.74"
+        }
+      },
+      {
+        "o": {
+          "type": "literal",
+          "value": "Cultural Affairs"
+        },
+        "avgSal": {
+          "type": "literal",
+          "datatype": "http://www.w3.org/2001/XMLSchema#decimal",
+          "value": "29.81"
+        }
+      },
+      {
+        "o": {
+          "type": "literal",
+          "value": "General Services"
+        },
+        "avgSal": {
+          "type": "literal",
+          "datatype": "http://www.w3.org/2001/XMLSchema#decimal",
+          "value": "31.7706"
+        }
+      },
+      {
+        "o": {
+          "type": "literal",
+          "value": "Information Technology Agency"
+        },
+        "avgSal": {
+          "type": "literal",
+          "datatype": "http://www.w3.org/2001/XMLSchema#decimal",
+          "value": "50.796"
+        }
+      },
+      {
+        "o": {
+          "type": "literal",
+          "value": "Transportation"
+        },
+        "avgSal": {
+          "type": "literal",
+          "datatype": "http://www.w3.org/2001/XMLSchema#decimal",
+          "value": "31.838666666666666666666666"
+        }
+      }
+    ]
+  }
+}
+
+
+;
+$(document).ready(function(){
+    $("#onClick").click(function(){
+        console.log("oh yeah");
+
+
+        barChart(my_json);
+
+    });
+});
+
+
+
+
+function barChart(my_json){
+var mydata = [];
+for (var i=0; i<my_json.results.bindings.length; i++){
+//console.log(my_json.results.bindings[i]);
+//  console.log(my_json.results.bindings[i].o.value);
+//  console.log(my_json.results.bindings[i].avgSal.value);
+
+
+  // x_bar.push(my_json.results.bindings[i].o.value);
+  // y_bar.push(my_json.results.bindings[i].avgSal.value);
+mydata.push({x:my_json.results.bindings[i].o.value, y:my_json.results.bindings[i].avgSal.value});
+
+}
+for (var i=0; i<mydata.length; i++){
+// console.log(mydata[i].x);
+// console.log(mydata[i].y);
+}
+
+       // mydata.forEach(function (d) {
+       //
+       //   //console.log(d.x);
+       //   //console.log(d.y);
+       //     d.x = +d.x;
+       //     d.y = +d.y;
+       // });
+
+
+  var svg = d3.select("svg"),
+      margin = {top: 10, right: 20, bottom: 220, left: 90},
+      width = +svg.attr("width") - margin.left - margin.right,
+      height = +svg.attr("height") - margin.top - margin.bottom;
+
+  var x = d3.scaleBand().rangeRound([0, width]).padding(0.1),
+      y = d3.scaleLinear().rangeRound([height, 0]);
+
+  var g = svg.append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+
+    x.domain(mydata.map(function(d,i) {
+console.log(d.x);
+      return d.x;
+
+     }));
+    y.domain([0, d3.max(mydata, function(d) {
+console.log(d.y);
+      return d.y;
+
+    })]);
+
+    // g.append("g")
+    //     .attr("class", "axis axis--x")
+    //     .attr("transform", "translate(0," + height + ")")
+    //     .call(d3.axisBottom(x));
+
+        g.append("g")
+			.classed("axis axis--x", true)
+			.attr("transform", "translate(" + 0 + "," + height + ")")
+			.call(d3.axisBottom(x))
+				.selectAll("text")
+					.classed("x-axis-label", true)
+					.style("text-anchor", "end")
+          .attr("font-size","9px")
+					.attr("dx", -8)
+					.attr("dy", 8)
+					.attr("transform", "translate(0,0) rotate(-45)");
+
+    g.append("g")
+        .attr("class", "axis axis--y")
+        .call(d3.axisLeft(y).ticks(5))
+      .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", "0.71em")
+        .attr("text-anchor", "end")
+        .text("Average Salary");
+
+    g.selectAll(".bar")
+      .data(mydata)
+      .enter().append("rect")
+        .attr("class", "bar")
+        .attr("x", function(d) { return x(d.x); })
+        .attr("y", function(d) { return y(d.y); })
+        .attr("width", x.bandwidth())
+        .attr("height", function(d) { return height - y(d.y); });
+
+
+}
